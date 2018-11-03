b1bb8d43cce18fc311ea9747b0f7ceac81c90a4b
Lai Weng Han <jlwhoo7@gmail.com>
2018-11-04 01:13:43 +0800

    Seperate logic from codes

--- src/router/steemhunt/status.ts
+++ src/router/steemhunt/status.ts
@@ -2,66 +2,7 @@ import * as Discord from 'discord.js'
 import axios from 'axios'
 import { errorMsg, color } from '../../template'
 import * as distanceInWords from 'date-fns/distance_in_words'
-
-// source: https://github.com/Steemhunt/web/blob/4f3c11be8263629183e195f1c9df19c3c8bc0e3e/src/features/User/utils.js
-const influencer: string[] = [
-  'ogochukwu',
-  'elleok',
-  'themanualbot',
-  'chuuuckie',
-  'shaphir',
-  'mobi72',
-  'fruitdaddy',
-  'jonsnow1983',
-  'karamyog',
-  'elsiekjay',
-  'calprut',
-  'ninuola',
-  'sonrhey',
-  'dayjee',
-  'camzy',
-  'abasifreke',
-  'gentleshaid',
-  'aamirijaz',
-  'faithcalls',
-  'tio'
-]
-
-interface IApiData {
-  id: number
-  author: string
-  url: string
-  title: string
-  tagline: string
-  tags: string[]
-  images: { name: string; link: string }[]
-  beneficiaries: any[]
-  permlink: string
-  is_active: boolean
-  payout_value: number
-  active_votes: {
-    voter: string
-    weight: number
-    rshares: string
-    percent: number
-    reputation: string
-    time: string
-  }[]
-  children: number
-  created_at: string
-  updated_at: string
-  description: string
-  is_verified: boolean
-  verified_by: string
-  hunt_score: number
-  valid_votes: {
-    voter: string
-    percent: number
-    weight: number
-    score: number
-  }[]
-  listed_at: number
-}
+import { influencer, IApiData } from './types'
 
 export const postStatus = async (msg: Discord.Message, args: string, client: Discord.Client) => {
   let authorName, permlinkName
---
+++ src/router/steemhunt/types.ts
@@ -0,0 +1,59 @@
+// source: https://github.com/Steemhunt/web/blob/4f3c11be8263629183e195f1c9df19c3c8bc0e3e/src/features/User/utils.js
+export const influencer: string[] = [
+  'ogochukwu',
+  'elleok',
+  'themanualbot',
+  'chuuuckie',
+  'shaphir',
+  'mobi72',
+  'fruitdaddy',
+  'jonsnow1983',
+  'karamyog',
+  'elsiekjay',
+  'calprut',
+  'ninuola',
+  'sonrhey',
+  'dayjee',
+  'camzy',
+  'abasifreke',
+  'gentleshaid',
+  'aamirijaz',
+  'faithcalls',
+  'tio'
+]
+
+export interface IApiData {
+  id: number
+  author: string
+  url: string
+  title: string
+  tagline: string
+  tags: string[]
+  images: { name: string; link: string }[]
+  beneficiaries: any[]
+  permlink: string
+  is_active: boolean
+  payout_value: number
+  active_votes: {
+    voter: string
+    weight: number
+    rshares: string
+    percent: number
+    reputation: string
+    time: string
+  }[]
+  children: number
+  created_at: string
+  updated_at: string
+  description: string
+  is_verified: boolean
+  verified_by: string
+  hunt_score: number
+  valid_votes: {
+    voter: string
+    percent: number
+    weight: number
+    score: number
+  }[]
+  listed_at: number
+}

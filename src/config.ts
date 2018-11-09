b55244370eafa529f8fef32e3d2475297d14faf9
Lai Weng Han <jlwhoo7@gmail.com>
2018-11-09 18:08:01 +0800

    Added permission check for approved channel

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,22 @@
-const BOT_ID: string = ''
 const TRIGGER: string = '.'
+// only approved channel can use the bot
+const APPROVED_CHANNEL: string[] = [
+  // superoo7 discord
+  '439600597147320320', // #bot-test
+  '439581185304166401', // #bot
+  // teammalaysia
+  '398416816868818944', // #malaysia
+  '398418077382868995', // #singapore
+  '398423469618102272', // #australia
+  '398675523959652352', // #thailand
+  '424777499927642113', // #phillipines
+  '424777702042632202', // #uk
+  // Aus & NZ
+  '436190850872115200', // #superoo7-crypto-bot
+  // Steemhunt
+  '509941015021617181', // #stats-bot
+  // Steemph.Cebu
+  '416961201680154625' // #crypto-price-feed-channel
+]
 
-export { BOT_ID, TRIGGER }
+export { TRIGGER, APPROVED_CHANNEL }
